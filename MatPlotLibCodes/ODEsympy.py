c943eab0253c5243caaa7108c832a29dfb70d47f
Taku Nakai <t.nakai.office@gmail.com>
2020-03-09 07:06:46 +0900

    format

--- MatPlotLibCodes/ODEsympy.py
+++ MatPlotLibCodes/ODEsympy.py
@@ -8,14 +8,14 @@
 
 from sympy import *
 
-f, g = symbols('f g',cls=Function)      # Make f a function 
+f, g = symbols('f g', cls=Function)      # Make f a function
 t, kap, w0 = symbols('t kap w0')
 f(t)
 f(t).diff(t)
-print( "\n ODE to be solved:" )
-diffeq = Eq(f(t).diff(t,t) + kap*(f(t).diff(t)) + (w0*w0)*f(t))
-print( diffeq)
-print( "\n Solution of ODE:")
-ff = dsolve(diffeq,f(t))                         # Solves ODE
-F = ff.subs(t,0)
-print( ff)
+print("\n ODE to be solved:")
+diffeq = Eq(f(t).diff(t, t) + kap * (f(t).diff(t)) + (w0 * w0) * f(t))
+print(diffeq)
+print("\n Solution of ODE:")
+ff = dsolve(diffeq, f(t))                         # Solves ODE
+F = ff.subs(t, 0)
+print(ff)
