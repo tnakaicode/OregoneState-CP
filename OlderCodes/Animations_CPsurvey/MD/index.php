0c0b61a5a55393d5ac5b2984520bbf1b0a3602b6
Taku Nakai <t.nakai.office@gmail.com>
2020-03-08 21:09:45 +0900

    rename

---
+++ OlderCodes/Animations_CPsurvey/MD/index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>Directory Listings of <? echo $_SERVER["REQUEST_URI"]; ?> </title>
+<style type='text/css'>
+<!--
+body {     color: <? echo $textcolor; ?>; font: tahoma, small verdana,arial,helvetica,sans-serif; background-color: <? echo $bgcolor; ?>; }
+table { font-family: tahoma, Verdana, Geneva, sans-serif; font-size: 10pt; border: 1px; border-style: solid; border-color: <? echo $bordercolor; ?>; }
+.row { background-color: <? echo $normalcolor; ?>; border: 0px;}
+a:link { color: <? echo $textcolor; ?>;  text-decoration: none; } 
+a:visited { color: <? echo $textcolor; ?>;  text-decoration: none; } 
+a:hover, a:active { color: <? echo $textcolor; ?>;  text-decoration: underline; } 
+img {border: 0;}
+#bottomborder {border: <? echo $bordercolor;?>;border-style: solid;border-top-width: 0px;border-right-width: 0px;border-bottom-width: 1px;border-left-width: 0px}
+.copy { text-align: center; color: <? echo $textcolor; ?>; font-family: tahoma, Verdana, Geneva, sans-serif;  font-size: 10pt; text-decoration: underline; }
+//-->
+</style>
+</head>
+<body>
+<table width="450" border="0" cellspacing="0" align="center"><tr bgcolor="#FFFFFF"><td colspan="2" id="bottomborder"><a href="/~landaur/Books/Problems/Codes/OlderCodes/Animations(CPsurvey)/MD/index.php?sort=name&order=asc">File</td><td id="bottomborder" width="50"></a><a href="/~landaur/Books/Problems/Codes/OlderCodes/Animations(CPsurvey)/MD/index.php?sort=size&order=asc">Size</td><td id="bottomborder" width="120" nowrap></a><a href="/~landaur/Books/Problems/Codes/OlderCodes/Animations(CPsurvey)/MD/index.php?sort=date&order=asc">Date Modified</a></td></tr>	<tr class="row" onMouseOver="this.style.backgroundColor='#FFFFFF'; this.style.cursor='hand';" onMouseOut="this.style.backgroundColor='#FFFFFF';" onClick="window.location.href='index.php';">
+		<td width="16"></td>
+		<td><a href="index.php">index.php</a></td>
+		<td width="50" align="left">5KB</td>
+		<td width="120" align="left" nowrap>Jul 14 2015 05:36:50 PM</td>
+	</tr>
+	<tr class="row" onMouseOver="this.style.backgroundColor='#FFFFFF'; this.style.cursor='hand';" onMouseOut="this.style.backgroundColor='#FFFFFF';" onClick="window.location.href='MD2d_25particles.avi';">
+		<td width="16"></td>
+		<td><a href="MD2d_25particles.avi">MD2d_25particles.avi</a></td>
+		<td width="50" align="left">4540KB</td>
+		<td width="120" align="left" nowrap>Jul 14 2015 05:36:50 PM</td>
+	</tr>
+	<tr class="row" onMouseOver="this.style.backgroundColor='#FFFFFF'; this.style.cursor='hand';" onMouseOut="this.style.backgroundColor='#FFFFFF';" onClick="window.location.href='md2d_100.avi';">
+		<td width="16"></td>
+		<td><a href="md2d_100.avi">md2d_100.avi</a></td>
+		<td width="50" align="left">11266KB</td>
+		<td width="120" align="left" nowrap>Jul 14 2015 05:36:50 PM</td>
+	</tr>
+	<tr class="row" onMouseOver="this.style.backgroundColor='#FFFFFF'; this.style.cursor='hand';" onMouseOut="this.style.backgroundColor='#FFFFFF';" onClick="window.location.href='MDanimate2D4particle.gif';">
+		<td width="16"></td>
+		<td><a href="MDanimate2D4particle.gif">MDanimate2D4particle.gif</a></td>
+		<td width="50" align="left">28KB</td>
+		<td width="120" align="left" nowrap>Jul 14 2015 05:36:50 PM</td>
+	</tr>
+	<tr class="row" onMouseOver="this.style.backgroundColor='#FFFFFF'; this.style.cursor='hand';" onMouseOut="this.style.backgroundColor='#FFFFFF';" onClick="window.location.href='Thumbs.db';">
+		<td width="16"></td>
+		<td><a href="Thumbs.db">Thumbs.db</a></td>
+		<td width="50" align="left">9KB</td>
+		<td width="120" align="left" nowrap>Jul 14 2015 05:36:50 PM</td>
+	</tr>
+</body>
+</html>
