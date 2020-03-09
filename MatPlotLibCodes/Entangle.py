5a7465698699468ddf97daf608fb4e0ed341191f
Taku Nakai <tnakaicode@gmail.com>
2020-03-09 12:27:12 +0900

    format

--- MatPlotLibCodes/Entangle.py
+++ MatPlotLibCodes/Entangle.py
@@ -6,27 +6,28 @@
 
 # Entangle.py: Calculate quantum entangled states
 
-from numpy import * ; from numpy.linalg import *
+from numpy import *
+from numpy.linalg import *
 
 nmax = 4
-H = zeros((nmax,nmax),float)
-XAXB = array([[0, 0, 0, 1],[0,0,1,0],[0,1,0,0],[1,0,0,0]])
-YAYB = array([[0,0,0,-1],[0,0,1,0],[0,1,0,0],[-1,0,0,0]])
-ZAZB = array([[1,0,0,0],[0,-1,0,0],[0,0,-1,0],[0,0,0,1]])
-SASB = XAXB + YAYB + ZAZB - 3*ZAZB       # Hamiltonian 
-print( '\nHamiltonian without mu^2/r^3 factor \n', SASB, '\n')
- 
-es,ev = eig(SASB)            # Eigenvalues and eigenvectors
-print ('Eigenvalues\n', es, '\n')
-print( "Eigenvectors (in columns)\n", ev, "\n")
-phi1 = (ev[0,0],ev[1,0],ev[2,0],ev[3,0])  # Eigenvectors
-phi4 = (ev[0,1],ev[1,1],ev[2,1],ev[3,1])  
-phi3 = (ev[0,2],ev[1,2],ev[2,2],ev[3,2])
-phi2 = (ev[0,3],ev[1,3],ev[2,3],ev[3,3])
-basis=[phi1, phi2, phi3,phi4]        # List eigenvectors
+H = zeros((nmax, nmax), float)
+XAXB = array([[0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 0, 0, 0]])
+YAYB = array([[0, 0, 0, -1], [0, 0, 1, 0], [0, 1, 0, 0], [-1, 0, 0, 0]])
+ZAZB = array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+SASB = XAXB + YAYB + ZAZB - 3 * ZAZB       # Hamiltonian
+print('\nHamiltonian without mu^2/r^3 factor \n', SASB, '\n')
 
-for i in range(0,nmax):         # Hamiltonian in new basis
-    for j in range(0,nmax):
-       term   = dot(SASB,basis[i])
-       H[i,j] = dot(basis[j],term)
-print( "Hamiltonian in Eigenvector Basis\n", H    )
+es, ev = eig(SASB)            # Eigenvalues and eigenvectors
+print('Eigenvalues\n', es, '\n')
+print("Eigenvectors (in columns)\n", ev, "\n")
+phi1 = (ev[0, 0], ev[1, 0], ev[2, 0], ev[3, 0])  # Eigenvectors
+phi4 = (ev[0, 1], ev[1, 1], ev[2, 1], ev[3, 1])
+phi3 = (ev[0, 2], ev[1, 2], ev[2, 2], ev[3, 2])
+phi2 = (ev[0, 3], ev[1, 3], ev[2, 3], ev[3, 3])
+basis = [phi1, phi2, phi3, phi4]        # List eigenvectors
+
+for i in range(0, nmax):         # Hamiltonian in new basis
+    for j in range(0, nmax):
+        term = dot(SASB, basis[i])
+        H[i, j] = dot(basis[j], term)
+print("Hamiltonian in Eigenvector Basis\n", H)
--- MatPlotLibCodes/EqHeatMov.py
+++ MatPlotLibCodes/EqHeatMov.py
@@ -3,41 +3,56 @@
     Copyright R Landau, Oregon State Unv, MJ Paez, Univ Antioquia, 
     C Bordeianu, Univ Bucharest, 2018. 
     Please respect copyright & acknowledge our work."""
-   
+
 # EqHeatMov.py: Matplotlib, animated heat eqn via finite diffs
 
 from numpy import *
-import numpy as np, matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
- 
-Nx = 101;   Dx = 0.01414;   Dt = 0.6                                                                 
-kappa = 210.;  sph = 900.; rho = 2700.                                         
-cons = kappa/(sph*rho)*Dt/(Dx*Dx)   # For algorthim
-T = np.zeros( (Nx, 2), float)       # Temp @ first 2 times
+
+Nx = 101
+Dx = 0.01414
+Dt = 0.6
+kappa = 210.
+sph = 900.
+rho = 2700.
+cons = kappa / (sph * rho) * Dt / (Dx * Dx)   # For algorthim
+T = np.zeros((Nx, 2), float)       # Temp @ first 2 times
+
 
 def init():
-   for ix in range (1, Nx - 1):   T[ix, 0] = 100.0 # Initial T
-   T[0, 0] = 0.0;  T[0, 1] = 0.              # Bar ends, t = 0    
-   T[Nx - 1, 0] = 0.;  T[Nx - 1, 1] = 0.0
-   
+    for ix in range(1, Nx - 1):
+        T[ix, 0] = 100.0  # Initial T
+    T[0, 0] = 0.0
+    T[0, 1] = 0.              # Bar ends, t = 0
+    T[Nx - 1, 0] = 0.
+    T[Nx - 1, 1] = 0.0
+
+
 init()
-k = range(0,Nx)
-fig = plt.figure()        
+k = range(0, Nx)
+fig = plt.figure()
 # select axis; 111: only one plot, x,y, scales given
-ax = fig.add_subplot(111, autoscale_on=False, xlim=(-5, 105), 
-	ylim=(-5, 110.0))
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-5, 105),
+                     ylim=(-5, 110.0))
 ax.grid()                                      # Plot a grid
 plt.ylabel("Temperature")      		# Temperature of  bar
 plt.title("Cooling of a bar")
-line, = ax.plot(k, T[k,0],"r", lw=2)      
-plt.plot([1,99],[0,0],"r",lw=10)         # Bar
-plt.text(45,5,'bar',fontsize=20)         
- 
+line, = ax.plot(k, T[k, 0], "r", lw=2)
+plt.plot([1, 99], [0, 0], "r", lw=10)         # Bar
+plt.text(45, 5, 'bar', fontsize=20)
+
+
 def animate(dum):                    # Dummy num for animation
-   for ix in range (1, Nx-1):    
-      T[ix, 1] = T[ix, 0] + cons*(T[ix+1,0]+T[ix-1,0]-2*T[ix, 0])
-   line.set_data(k,T[k,1] )    # (x, y)=(k, temperature) 
-   for ix in range (1, Nx-1):  T[ix,0] = T[ix,1]  # 100 positions  
-   return line,            
-ani = animation.FuncAnimation(fig, animate,1)            # 1=dummy
+    for ix in range(1, Nx - 1):
+        T[ix, 1] = T[ix, 0] + cons * \
+            (T[ix + 1, 0] + T[ix - 1, 0] - 2 * T[ix, 0])
+    line.set_data(k, T[k, 1])    # (x, y)=(k, temperature)
+    for ix in range(1, Nx - 1):
+        T[ix, 0] = T[ix, 1]  # 100 positions
+    return line,
+
+
+ani = animation.FuncAnimation(fig, animate, 1)            # 1=dummy
 plt.show()
