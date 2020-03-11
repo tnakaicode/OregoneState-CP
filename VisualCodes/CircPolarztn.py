cac2e0c625a40fe0b966932333a82ca88e2100be
Taku Nakai <tnakaicode@gmail.com>
2020-03-11 16:41:25 +0900

    fomat

--- VisualCodes/CircPolarztn.py
+++ VisualCodes/CircPolarztn.py
@@ -4,56 +4,80 @@
     C Bordeianu, Univ Bucharest, 2018. 
     Please respect copyright & acknowledge our work."""
 
-# CircPolarztn.py: Maxwell eqs. circular polariz via FDTD 
+# CircPolarztn.py: Maxwell eqs. circular polariz via FDTD
 
-from vpython import *  
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import time
+import os
 
-scene = display(x=0,y=0,width=600,height=400,range=200,
-	title='Circular Polarized E(white)& H(yellow)Fields')
-global phy, pyx
-max = 201; c = 0.01                 # Courant stable if c < 0.1                                             
-Ex = zeros((max+2,2),float);   Ey = zeros((max+2,2),float)                      
-Hy = zeros((max+2,2),float);   Hx = zeros((max+2,2),float) 
-arrowcol= color.white                                   
-Earrows = [];                  Harrows = []
-for i in range(0,max,10):       
-    Earrows.append(arrow(pos=(0,i-100,0), axis=(0,0,0), 
-    	    color=arrowcol))
-    Harrows.append(arrow(pos=(0,i-100,0), axis=(0,0,0), 
-    	    color=color.yellow))
+sys.path.append(os.path.join('../'))
+from base import plot2d
 
-def plotfields(Ex,Ey,Hx,Hy):
+phy, pyx = 0.0, 0.0
+max = 201
+c = 0.01                 # Courant stable if c < 0.1
+Ex = np.zeros((max + 2, 2), float)
+Ey = np.zeros((max + 2, 2), float)
+Hy = np.zeros((max + 2, 2), float)
+Hx = np.zeros((max + 2, 2), float)
+arrowcol = color.white
+Earrows = []
+Harrows = []
+for i in range(0, max, 10):
+    Earrows.append(arrow(pos=(0, i - 100, 0), axis=(0, 0, 0),
+                         color=arrowcol))
+    Harrows.append(arrow(pos=(0, i - 100, 0), axis=(0, 0, 0),
+                         color=color.yellow))
+
+
+def plotfields(Ex, Ey, Hx, Hy):
     for n, arr in enumerate(Earrows):
-        arr.axis = (35*Ey[10*n,1],0,35*Ex[10*n,1])
+        arr.axis = (35 * Ey[10 * n, 1], 0, 35 * Ex[10 * n, 1])
     for n, arr in enumerate(Harrows):
-        arr.axis = (35*Hy[10*n,1],0,35*Hx[10*n,1])
+        arr.axis = (35 * Hy[10 * n, 1], 0, 35 * Hx[10 * n, 1])
 
-def inifields():                     # Initial E & H
-    phx = 0.5*pi;  phy = 0.0
-    z = arange(0,max)
-    Ex[:-2,0] = cos(-2*pi*z/200+phx)
-    Ey[:-2,0] = cos(-2*pi*z/200+phy)
-    Hx[:-2,0] = cos(-2*pi*z/200+phy+pi)
-    Hy[:-2,0] = cos(-2*pi*z/200+phx)
-        
+
+def inifields():
+    # Initial E & H
+    phx = 0.5 * np.pi
+    phy = 0.0
+    z = np.arange(0, max)
+    Ex[:-2, 0] = np.cos(-2 * np.pi * z / 200 + phx)
+    Ey[:-2, 0] = np.cos(-2 * np.pi * z / 200 + phy)
+    Hx[:-2, 0] = np.cos(-2 * np.pi * z / 200 + phy + np.pi)
+    Hy[:-2, 0] = np.cos(-2 * np.pi * z / 200 + phx)
+
+
 def newfields():
-  while True:                           # Time stepping
-    rate(1000)
-    Ex[1:max-1,1] =Ex[1:max-1,0] + c*(Hy[:max-2,0]-Hy[2:max,0])  
-    Ey[1:max-1,1] =Ey[1:max-1,0] + c*(Hx[2:max,0]-Hx[:max-2,0])
-    Hx[1:max-1,1] =Hx[1:max-1,0] + c*(Ey[2:max,0]-Ey[:max-2,0]) 
-    Hy[1:max-1,1] =Hy[1:max-1,0] + c*(Ex[:max-2,0]-Ex[2:max,0])
-    Ex[0,1]   = Ex[0,0]   + c*(Hy[200-1,0]-Hy[1,0]) # Periodic     
-    Ex[200,1] = Ex[200,0] + c*(Hy[200-1,0]-Hy[1,0])    
-    Ey[0,1]   = Ey[0,0]   + c*(Hx[1,0]- Hx[200-1,0])   
-    Ey[200,1] = Ey[200,0] + c*(Hx[1,0]- Hx[200-1,0])
-    Hx[0,1]   = Hx[0,0]   + c*(Ey[1,0]- Ey[200-1,0])
-    Hx[200,1] = Hx[200,0] + c*(Ey[1,0]- Ey[200-1,0])
-    Hy[0,1]   = Hy[0,0]   + c*(Ex[200-1,0]-Ex[1,0])
-    Hy[200,1] = Hy[200,0] + c*(Ex[200-1,0]-Ex[1,0])
-    plotfields(Ex,Ey,Hx,Hy)
-    Ex[:max,0] = Ex[:max,1];  Ey[:max,0] = Ey[:max,1] # Update
-    Hx[:max,0] = Hx[:max,1];  Hy[:max,0] = Hy[:max,1]
-          
-inifields()                                          # Initial
-newfields()                                          # New 
+    while True:
+        # Time stepping
+        Ex[1:max - 1, 1] = Ex[1:max - 1, 0] + \
+            c * (Hy[:max - 2, 0] - Hy[2:max, 0])
+        Ey[1:max - 1, 1] = Ey[1:max - 1, 0] + \
+            c * (Hx[2:max, 0] - Hx[:max - 2, 0])
+        Hx[1:max - 1, 1] = Hx[1:max - 1, 0] + \
+            c * (Ey[2:max, 0] - Ey[:max - 2, 0])
+        Hy[1:max - 1, 1] = Hy[1:max - 1, 0] + \
+            c * (Ex[:max - 2, 0] - Ex[2:max, 0])
+        Ex[0, 1] = Ex[0, 0] + c * (Hy[200 - 1, 0] - Hy[1, 0])
+
+        # Periodic
+        Ex[200, 1] = Ex[200, 0] + c * (Hy[200 - 1, 0] - Hy[1, 0])
+        Ey[0, 1] = Ey[0, 0] + c * (Hx[1, 0] - Hx[200 - 1, 0])
+        Ey[200, 1] = Ey[200, 0] + c * (Hx[1, 0] - Hx[200 - 1, 0])
+        Hx[0, 1] = Hx[0, 0] + c * (Ey[1, 0] - Ey[200 - 1, 0])
+        Hx[200, 1] = Hx[200, 0] + c * (Ey[1, 0] - Ey[200 - 1, 0])
+        Hy[0, 1] = Hy[0, 0] + c * (Ex[200 - 1, 0] - Ex[1, 0])
+        Hy[200, 1] = Hy[200, 0] + c * (Ex[200 - 1, 0] - Ex[1, 0])
+        plotfields(Ex, Ey, Hx, Hy)
+
+        Ex[:max, 0] = Ex[:max, 1]
+        Ey[:max, 0] = Ey[:max, 1]
+        Hx[:max, 0] = Hx[:max, 1]
+        Hy[:max, 0] = Hy[:max, 1]
+
+
+inifields()
+newfields()
--- VisualCodes/DecaySoundVis.py
+++ VisualCodes/DecaySoundVis.py
@@ -6,22 +6,32 @@
 
 # DecaySoundVis.py: Visual, spontaneous decay simulation
 
-from vpython import *; from vpython.graph import *
-import random, winsound
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import winsound
+import sys
+import time
+import os
 
-lambda1 = 0.005                               # Decay constant                      
-max = 80.;  time_max = 500;   seed = 68111                                    
+sys.path.append(os.path.join('../'))
+from base import plot2d
+
+lambda1 = 0.005                               # Decay constant
+max = 80.
+time_max = 500
+seed = 68111
 number = nloop = max                           # Initial value
-graph1 = gdisplay(title ='Spontaneous Decay',xtitle='Time',
-                  ytitle = 'Number')
-decayfunc = gcurve(color = color.green)
+graph1 = gdisplay(title='Spontaneous Decay', xtitle='Time',
+                  ytitle='Number')
+decayfunc = gcurve(color=color.green)
 
-for time in arange(0, time_max + 1):              # Time loop
-    for atom in arange(1, number + 1 ):          # Decay loop
-        decay = random.random()   
-        if (decay  <  lambda1):
-            nloop = nloop  -  1                     # A decay
+for time in np.arange(0, time_max + 1):              # Time loop
+    for atom in np.arange(1, number + 1):          # Decay loop
+        decay = random.random()
+        if (decay < lambda1):
+            nloop = nloop - 1                     # A decay
             winsound.Beep(600, 100)              # Sound beep
     number = nloop
-    decayfunc.plot( pos = (time, number) )
+    decayfunc.plot(pos=(time, number))
     rate(30)
